fbc4b501dfb51cbf1d1544de7ce81d2f9f2be185
Alby-On <66146586+Alby-On@users.noreply.github.com>
2026-03-15 18:22:06 -0300

    Update components.js

--- js/components.js
+++ js/components.js
@@ -41,11 +41,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // Definimos los componentes en un array para facilitar el mantenimiento futuro
     const componentes = [
         { id: 'header', path: 'components/header.html' },
-        { id: 'nosotros', path: 'pages/nosotros.html' },
+        { id: 'contenedor-nosotros', path: 'pages/nosotros.html' },
         { id: 'contenedor-contacto', path: 'pages/contacto.html' },
-        { id: 'proyectos', path: 'pages/proyectos.html' },
-        { id: 'planes', path: 'pages/planes.html' },
-        { id: 'servicios', path: 'pages/servicios.html' },
+        { id: 'contenedor-proyectos', path: 'pages/proyectos.html' },
+        { id: 'contenedor-planes', path: 'pages/planes.html' },
+        { id: 'contenedor-servicios', path: 'pages/servicios.html' },
         { id: 'footer', path: 'components/footer.html' },
         { id: 'wsp-button', path: 'components/wsp-button.html' }
     ];
